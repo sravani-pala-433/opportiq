7bd29851ab357f72085b00edd305c69679083e9f
sravani433 <sravani@prodifyllp.com>
2026-09-24 23:09:39 +0530

    Add cryptography dependency

--- core/config/settings.py
+++ core/config/settings.py
@@ -18,6 +18,7 @@ class Settings(BaseSettings):
         env_file = '.env'
         env_file_encoding = 'utf-8'
         case_sensitive = False
+        extra = 'ignore'
 
 @lru_cache()
 def get_settings():
